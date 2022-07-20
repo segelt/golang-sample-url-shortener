a9c94f4146fdaa8212e5afdc43b5bd1587c04a8c
segelt <86981324+segelt@users.noreply.github.com>
2022-07-20 23:12:25 +0300

    main server implementing hash endpoint

--- src/main.go
+++ src/main.go
@@ -1,10 +1,24 @@
 package main
 
-// import (
-// 	"fmt"
-// 	"gobasictinyurl/src/hashutility"
-// )
+import (
+	"fmt"
+	"gobasictinyurl/src/hashutility"
+	"gobasictinyurl/src/server"
+	"net/http"
+)
 
 func main() {
-	panic("Not implented..")
+	mux := http.NewServeMux()
+	srv := server.New(mux, ":3333")
+
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		endpoint := r.URL.Query().Get("endpoint")
+		w.Write([]byte(hashutility.GetNextHashSeq(endpoint)))
+	})
+
+	err := srv.ListenAndServe()
+
+	if err != nil {
+		fmt.Println("Error starting the server...")
+	}
 }
